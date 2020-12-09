ba4027ed2dd592469319696db82b954f7ec0aa15
Jake Lane <lanejake13@gmail.com>
2020-12-09 12:47:24 -0500

    Add project files.

---
+++ LeagueOfLegends/APIRepository.cs
@@ -0,0 +1,50 @@
+﻿using LeagueOfLegends.Models;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegends
+{
+    public static class APIRepository
+    {
+        public static Summoner GetSummonerID(string summonerName)
+        {
+            var url = $"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=RGAPI-ee6ef0c5-f0f8-4aee-9bcf-1746760b98db";
+            
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+        
+            IRestResponse response = client.Execute(request);
+
+            var data = JObject.Parse(response.Content);
+
+            var summoner = new Summoner();
+            summoner.SummonerID = (string)data["id"];
+
+            return summoner;
+        }
+
+        public static Summoner GetPlayerStats(this Summoner summoner)
+        {
+            var url = $"https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner.SummonerID}?api_key=RGAPI-ee6ef0c5-f0f8-4aee-9bcf-1746760b98db";
+
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            //request.AddHeader("x-rapidapi-key", "75659707a4mshbe175a73468d4e2p1b9494jsn356396c32494");
+            //request.AddHeader("x-rapidapi-host", "amazon-product-reviews-keywords.p.rapidapi.com");
+            IRestResponse response = client.Execute(request);
+
+            var data = JArray.Parse(response.Content);
+
+
+            summoner.Wins = (int)data[0]["wins"];
+            summoner.Losses = (int)data[0]["losses"];
+            summoner.Tier = (string)data[0]["tier"];
+
+            return summoner;
+        }
+    }
+}
---
+++ LeagueOfLegends/Models/Summoner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegends.Models
+{
+    public class Summoner
+    {
+        /* "leagueId": "f614ff48-e79d-4f78-af76-c8a733533a17",
+        "queueType": "RANKED_SOLO_5x5",
+        "tier": "GOLD",
+        "rank": "III",
+        "summonerId": "sr4qsPjllF0-a7HrxdO85c4Lp2VDKEHoDwQvVUTTRjzbM6k",
+        "summonerName": "Dopas Son",
+        "leaguePoints": 47,
+        "wins": 318,
+        "losses": 327,
+        "veteran": false,
+        "inactive": false,
+        "freshBlood": false,
+        "hotStreak": false*/
+
+        public string SummonerID { get; set; }
+        public string SummonerName { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public string Tier { get; set; }
+    }
+}
